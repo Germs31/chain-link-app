2ecb6a3b745afa23d0005c783fcb17984efc1f1f
German Nunez <german90nunez@gmail.com>
2020-02-19 19:01:58 -0800

    link price img and name rendered

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <div class="container">
-
+    <div  v-for="crypto in cryptos" v-bind:key="crypto.id">
+      <img width="100px" height="100px" v-bind:src="crypto.logo_url" />
+      <h1>{{crypto.currency}}</h1>
+      <h2>{{crypto.price}}</h2>
     </div>
   </div>
 </template>
@@ -11,15 +13,16 @@ import axios from 'axios'
 export default {
  data(){
    return {
-     crypto: []
+     cryptos: [],
+     title: 'hello'
    }
  },
  async created() {
    try {
      const cryptoCur = await axios.get(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.cryptId}&ids=LINK`);
 
-     this.crypto = cryptoCur.data
-     console.log(this.crypto)
+     this.cryptos = cryptoCur.data
+     console.log(this.cryptos)
 
    } catch (error) {
      console.log(error);
