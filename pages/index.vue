aff412d2f92a608def3077438a6ac0bc21ec1f6d
German Nunez <german90nunez@gmail.com>
2020-02-20 17:47:09 -0800

    link status set and reddit card component created

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,50 @@
 <template>
   <div>
-    <div  v-for="crypto in cryptos" v-bind:key="crypto.id">
-      <img width="100px" height="100px" v-bind:src="crypto.logo_url" />
+    <div class="container" v-for="crypto in cryptos" v-bind:key="crypto.id">
       <h1>{{crypto.currency}}</h1>
+      <img width="100px" height="100px" v-bind:src="crypto.logo_url" />
       <h2>{{crypto.price}}</h2>
+    </div>
+    <!-- card  -->
+    <div class="reddit-card-container">
+      <RedditCard/>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import RedditCard from '@/components/RedditCard'
 export default {
- data(){
-   return {
-     cryptos: [],
-     title: 'hello'
-   }
- },
- async created() {
-   try {
-     const cryptoCur = await axios.get(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.cryptId}&ids=LINK`);
+  components: {
+    RedditCard
+  },
+  data(){
+    return {
+      cryptos: [],
+      title: 'hello'
+    }
+  },
+  async created() {
+    try {
+      const cryptoCur = await axios.get(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.cryptId}&ids=LINK`);
 
-     this.cryptos = cryptoCur.data
-     console.log(this.cryptos)
+      this.cryptos = cryptoCur.data
+      console.log(this.cryptos)
 
-   } catch (error) {
-     console.log(error);
-   }
- }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 </script>
+
+<style>
+.container{
+  height: 25vh;
+  background: #3a4660;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
