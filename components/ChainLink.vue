ec35f58de4dc17638299c719abcd06c7940ec570
German Nunez <german90nunez@gmail.com>
2020-02-24 16:25:37 -0800

    mobile responsive

--- components/ChainLink.vue
+++ components/ChainLink.vue
@@ -9,6 +9,7 @@
             Today, the solution to this problem is to introduce a new functionality, called an oracle, that provides connectivity to the outside world. However, oracles to-date are centralized services, meaning any smart contract using such services has a single point of failure, which nullifies any benefits gained from the decentralized nature of smart contracts.
 
             To fill this gap, Chainlink was developed as the first decentralized oracle that can provide external data to smart contracts. As a result, the security and determinism of smart contracts can be combined with the knowledge and breadth of real-world external events. Chainlink will provide your smart contract with access to any external API you want to connect your smart contract with, and all the information needed to do so are listed here.</p>
+            <v-btn large href="https://chain.link/" target="_blank">Link</v-btn>
         </v-flex> 
     </div>
 </template>
